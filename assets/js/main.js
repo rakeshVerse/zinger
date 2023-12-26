94588749caacb2513f8e917ecbb736679a3469a6
fab-rakesh <ed.rakesh.ramteke@gmail.com>
2023-12-26 13:02:45 +0530

    Typehead: 1. click on typehead item

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,6 +34,9 @@ const addRecipeSubmitBtn = document.querySelector('#popup .btn-submit-recipe');
 
 const themeMenu = document.querySelector('.theme-item');
 
+const typeheadContainer = document.querySelector('.typehead-list');
+
+const searchForm = document.getElementById('search-form');
 const recipeSearchInput = document.getElementById('search-keyword');
 recipeSearchInput.value = '';
 
@@ -43,6 +46,150 @@ let totalRecipes;
 const savedRecipes = [];
 let recipeId = null;
 let servingsCounter;
+const allowedSearchKeywords = [
+  'buttermilk',
+  'butter',
+  'curd',
+  'milk',
+  'soup',
+  'noodles',
+  'dosa',
+  'bread',
+  'chana',
+  'dal',
+  'wheat',
+  'rice',
+  'burger',
+  'eggs',
+  'carrot',
+  'broccoli',
+  'asparagus',
+  'cauliflower',
+  'corn',
+  'cucumber',
+  'green pepper',
+  'lettuce',
+  'mushrooms',
+  'onion',
+  'potato',
+  'pumpkin',
+  'red pepper',
+  'tomato',
+  'beetroot',
+  'brussel sprouts',
+  'peas',
+  'zucchini',
+  'radish',
+  'sweet potato',
+  'artichoke',
+  'leek',
+  'cabbage',
+  'celery',
+  'chili',
+  'garlic',
+  'basil',
+  'coriander',
+  'parsley',
+  'dill',
+  'rosemary',
+  'oregano',
+  'cinnamon',
+  'saffron',
+  'green bean',
+  'bean',
+  'chickpea',
+  'lentil',
+  'apple',
+  'apricot',
+  'avocado',
+  'banana',
+  'blackberry',
+  'blackcurrant',
+  'blueberry',
+  'boysenberry',
+  'cherry',
+  'coconut',
+  'fig',
+  'grape',
+  'grapefruit',
+  'kiwifruit',
+  'lemon',
+  'lime',
+  'lychee',
+  'mandarin',
+  'mango',
+  'melon',
+  'nectarine',
+  'orange',
+  'papaya',
+  'passion fruit',
+  'peach',
+  'pear',
+  'pineapple',
+  'plum',
+  'pomegranate',
+  'quince',
+  'raspberry',
+  'strawberry',
+  'watermelon',
+  'salad',
+  'pizza',
+  'pasta',
+  'popcorn',
+  'lobster',
+  'steak',
+  'bbq',
+  'pudding',
+  'hamburger',
+  'pie',
+  'cake',
+  'sausage',
+  'tacos',
+  'kebab',
+  'poutine',
+  'seafood',
+  'chips',
+  'fries',
+  'masala',
+  'paella',
+  'som tam',
+  'chicken',
+  'toast',
+  'marzipan',
+  'tofu',
+  'ketchup',
+  'hummus',
+  'chili',
+  'maple syrup',
+  'parma ham',
+  'fajitas',
+  'champ',
+  'lasagna',
+  'poke',
+  'chocolate',
+  'croissant',
+  'arepas',
+  'bunny chow',
+  'pierogi',
+  'donuts',
+  'rendang',
+  'sushi',
+  'ice cream',
+  'duck',
+  'curry',
+  'beef',
+  'goat',
+  'lamb',
+  'turkey',
+  'pork',
+  'fish',
+  'crab',
+  'bacon',
+  'ham',
+  'pepperoni',
+  'salami',
+  'ribs',
+];
 
 /////////////// GENERAL ////////////////
 const AJAX = async (url, data = undefined) => {
@@ -88,6 +235,61 @@ themeMenu.addEventListener('click', function (e) {
 
   const themeClass = e.target.classList[1];
   document.body.className = themeClass;
+});
+
+/////////////// TYPEHEAD ////////////////
+
+const renderTypehead = keywords => {
+  let html = '';
+  keywords.forEach(keyword => (html += `<li class="typehead-item">${keyword}</li>`));
+
+  typeheadContainer.textContent = '';
+  typeheadContainer.style.height = 'fit-content';
+  typeheadContainer.insertAdjacentHTML('afterbegin', html);
+};
+
+const hideTypehead = () => {
+  // typeheadContainer.textContent = '';
+  typeheadContainer.style.height = 0;
+};
+
+// EVENTS
+
+// search input on-keyup
+recipeSearchInput.addEventListener('keyup', function (e) {
+  const key = e.key;
+  const input = recipeSearchInput.value;
+
+  if (!input || key === 'Enter' || key === 'Escape') {
+    hideTypehead();
+    return;
+  }
+
+  // search keyword in the array
+  const keywords = allowedSearchKeywords.filter(keyword => keyword.startsWith(input)).sort();
+
+  // if keyword not found, hide typehead
+  if (!keywords.length) {
+    hideTypehead();
+    return;
+  }
+
+  renderTypehead(keywords);
+});
+
+// when search input loses focus, hide typehead
+recipeSearchInput.addEventListener('blur', hideTypehead);
+
+// when click on typehead item, start search
+typeheadContainer.addEventListener('click', function (e) {
+  if (e.target === this) return;
+
+  hideTypehead();
+  const keyword = e.target.innerHTML;
+  if (!keyword) return;
+
+  recipeSearchInput.value = keyword;
+  searchRecipe(keyword);
 });
 
 /////////////// SEARCH RECIPE ////////////////
@@ -154,6 +356,9 @@ const searchRecipe = async keyword => {
     paginationBtnPrev.classList.add('hidden');
     paginationBtnNext.classList.add('hidden');
 
+    recipePreviewContainer.textContent = '';
+    showInfo(recipePreviewInfo, 'Searching...');
+
     const jsonData = await AJAX(`${FORKIFY_API_URL}?search=${keyword}&key=${FORKIFY_API_KEY}`);
     totalRecipes = jsonData.results;
 
@@ -184,16 +389,13 @@ const searchRecipe = async keyword => {
 // Events
 
 // Search
-document.getElementById('search-form').addEventListener('submit', function (e) {
+searchForm.addEventListener('submit', function (e) {
   e.preventDefault();
 
   const data = new FormData(this);
   const keyword = data.get('search-keyword').trim();
 
   if (keyword === '') return;
-
-  recipePreviewContainer.textContent = '';
-  showInfo(recipePreviewInfo, 'Searching...');
 
   searchRecipe(keyword);
 });
@@ -319,7 +521,7 @@ const renderRecipe = recipe => {
 
       <div class="recipe-actions">
         ${key ? '<span class="recipe-user opacity-6">👤</span>' : ''}
-        <a href="#" class="save-recipe">${isSavedRecipe(id) ? '❤️' : '🤍'}</a>
+        <a href="#" class="save-recipe opacity-6">${isSavedRecipe(id) ? '❤️' : '🤍'}</a>
       </div>
     </div>
 
